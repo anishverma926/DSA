702c251a7f76500c5c361035b3261cceff5d6c3e
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-12 08:02:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
-    int solve(int i, int n, vector<int>& arr, vector<vector<int>> & dp){
+    int solve(int i, int n, int x, vector<vector<int>>& dp){
         if(n == 0)
         return 1;
 
-        if(i == arr.size())
+        int val = pow(i, x); // 1^2, 2^2, 3^2, 4^2
+        if(val > n) // 16 > n
         return 0;
 
         if(dp[i][n] != -1)
         return dp[i][n];
 
-        int not_take = solve(i+1, n, arr, dp);
+        int not_take = solve(i+1, n, x, dp);
         int take = 0;
 
-        if(arr[i] <= n){
-            take = solve(i+1, n - arr[i], arr, dp);
+        if(val <= n){
+            take = solve(i+1, n - val, x, dp);
         }
 
-        return dp[i][n] = (take + not_take) % mod;
+        return dp[i][n] = take + not_take;
     }
     int numberOfWays(int n, int x) {
-        vector<int> arr;
-
-        for(int i = 1; i <= n; i++){
-            int val = pow(i, x);
-            if(val <= n)
-            arr.push_back(val);
-            else
-            break;
-        }
-
-        int m = arr.size();
-
-        vector<vector<int>> dp(m, vector<int> (n + 1, -1));
-
-        return solve(0, n, arr, dp);
+        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+        return solve(1, n, x, dp);
     }
 };
