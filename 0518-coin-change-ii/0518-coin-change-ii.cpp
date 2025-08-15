4cd2fd81f73e6a22d1b623057def27f26d7e1438
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-15 16:44:04 +0530

    Time: 24 ms (40.36%), Space: 28.2 MB (67.56%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    int solve(int i, vector<int>& coins, int amount, vector<vector<int>>& dp){
-        int n = coins.size();
-
-        if(amount == 0)
-        return 1;
-
-        if(i == n-1){
-            if(amount % coins[n-1] == 0)
-            return 1;
-            else
-            return 0;
+    long solve(int i, vector<int>& arr, int k, vector<vector<int>>& dp){
+        if (i == 0){  
+            return (k % arr[0] == 0);
         }
 
-        if(dp[i][amount] != -1)
-        return dp[i][amount];
+        if(dp[i][k] != -1)
+        return dp[i][k];
 
-        int not_take = solve(i + 1, coins, amount, dp);
-
-        int take = 0;
-        if(coins[i] <= amount){
-            take = solve(i, coins, amount - coins[i], dp);
+        long notPick = 0 + solve(i-1, arr, k, dp);
+        long pick = 0;
+        if(arr[i] <= k){
+            pick = solve(i, arr, k-arr[i], dp);
         }
 
-        return dp[i][amount] = take + not_take;
+        return dp[i][k] = pick + notPick;
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
+        vector<vector<int>>dp(n, vector<int>(amount+1, 0));
 
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        for(int T=0;T<=amount;T++){
+            dp[0][T] = (T % coins[0] == 0);
+        }
 
-        return solve(0, coins, amount, dp);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                long notPick = dp[i-1][j];
+                long pick = 0;
+                if(coins[i] <= j){
+                    pick = dp[i][j-coins[i]];
+                }
+                dp[i][j] = pick + notPick;
+
+            }
+        }
+        return dp[n-1][amount];
     }
 };
