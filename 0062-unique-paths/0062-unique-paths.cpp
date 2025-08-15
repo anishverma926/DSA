e4f574952d5bcecb9b8bc878e17daea3cf744cd0
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-15 23:12:22 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -17,7 +17,26 @@ public:
     }
 
     int uniquePaths(int a, int b) {
-        vector<vector<int>>dp(a, vector<int>(b,-1));
-        return countPath(a-1, b-1, dp);
+        vector<vector<int>>dp(a, vector<int>(b,0));
+        
+        dp[0][0] = 1;
+        
+        if(a < 0 || b < 0)
+        return 0;
+        
+        for(int i=0;i<a;i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int j=0;j<b;j++){
+            dp[0][j] = 1;
+        }
+        
+        for(int i=1;i<a;i++){
+            for(int j=1;j<b;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[a-1][b-1];
     }
 };
