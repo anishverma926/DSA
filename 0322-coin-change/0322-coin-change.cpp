db4b894d741279d85d6971bc9161938cd73cef75
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-15 12:10:26 +0530

    Time: 41 ms (48.75%), Space: 48.7 MB (48.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,34 +1,55 @@
 class Solution {
 public:
-    int solve(int i, vector<int>& coins, int amount, vector<vector<int>>& dp){
-        int n = coins.size();
 
-        if(i == n-1){
-            if(amount % coins[i] == 0)
-            return amount / coins[i];
-            else
+    int solve(int i, vector<int>&arr, int k, vector<vector<int>>dp){
+        if(k == 0)
+        return 0;
+
+        if(i == 0){
+            if(k % arr[i] == 0){
+                return k / arr[i];
+            }
             return 1e9;
         }
 
-        if(dp[i][amount] != -1)
-        return dp[i][amount];
+        if(dp[i][k] != -1)
+        return dp[i][k];
 
-        int not_take = solve(i + 1, coins, amount, dp);
-
-        int take = INT_MAX; // because we have to find minimum coins
-        if(coins[i] <= amount){
-            take = 1 + solve(i, coins, amount - coins[i], dp);
+        int notPick = solve(i-1, arr, k, dp);
+        int pick = INT_MAX;
+        if(arr[i] <= k){
+            pick = 1 + solve(i, arr, k - arr[i], dp);
         }
 
-        return dp[i][amount] = min(take, not_take);
+        return dp[i][k] = min(pick , notPick);
 
     }
-    int coinChange(vector<int>& coins, int amount) {
+
+    int coinChange(vector<int>& coins, int target) {
         int n = coins.size();
+        vector<vector<int>>dp(n, vector<int>(target+1, 0));
+        
+        for(int j=0;j<=target;j++){
+            if(j % coins[0] == 0){
+                dp[0][j] = j / coins[0];
+            }
+            else
+            dp[0][j] = 1e9;
+        }
+        for(int i=1;i<coins.size();i++){
+            for(int j=1;j<=target;j++){
+                int notPick = dp[i-1][j];
+                int pick = INT_MAX;
+                if(coins[i] <= j){
+                    pick = 1 + dp[i][j - coins[i]];
+                }
+                
+                dp[i][j] = min(pick , notPick);
+            }
+        }
 
-        vector<vector<int>> dp(n+1, vector<int>(amount + 1, -1));
-        int ans = solve(0, coins, amount, dp);
-
-        return ans >= 1e9 ? -1 : ans;
+        int ans = dp[n-1][target];
+        if(ans >= 1e9) return -1;
+        return ans;
     }
 };
