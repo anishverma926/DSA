d3eac5b2504787833344f660e8cfcd43115571f1
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-15 19:40:29 +0530

    Time: 3 ms (87.77%), Space: 15.2 MB (57.31%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,44 @@
 class Solution {
-public:
-    int solve(int i, vector<int>& nums, int target){
-        int n = nums.size();
+public: 
+int solve(int i, int target, vector<int>& arr, vector<vector<int>>& dp) {
+    if (i == 0) {
+        if (target == 0 && arr[0] == 0)
+        return 2; // Include or exclude the zero
+        if (target == 0 || target == arr[0])
+        return 1;
 
-        if(i == n){
-            if(target == 0)
-            return 1;
-            else
-            return 0;
-        }
-
-        int add = solve(i+1, nums, target - nums[i]);
-        int subtract = solve(i+1, nums, target + nums[i]);
-
-        return add + subtract;
+        return 0;
     }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
+    
+    if (dp[i][target] != -1)
+    return dp[i][target];
+    
+    int notPick = solve(i - 1, target, arr, dp);
+    int pick = 0;
+    if (arr[i] <= target) {
+        pick = solve(i - 1, target - arr[i], arr, dp);
+    }
+    
+    return dp[i][target] = (pick + notPick);
+}
+  
+int findWays(vector<int>& arr, int k) {
+    int n = arr.size();
+    vector<vector<int>> dp(n, vector<int>(k + 1, -1));
+    return solve(n - 1, k, arr, dp);
+}
 
-        return solve(0, nums,target);
+    int findTargetSumWays(vector<int>& arr, int target) {
+        int totalSum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            totalSum += arr[i];
+        }
+    
+        if (totalSum < target || (totalSum - target) % 2 != 0)
+        return 0;
+    
+        int sum = (totalSum - target) / 2;
+        return findWays(arr, sum);
+
     }
 };
