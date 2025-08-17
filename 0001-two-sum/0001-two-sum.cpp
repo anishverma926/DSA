eb2bfc158f0e15095950abcbc6df21bb748e359a
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-17 10:43:57 +0530

    Time: 0 ms (100%), Space: 14.8 MB (50.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,16 +2,32 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        int i = 0, j = 1;
-        map<int, int>mp;
-        for(int i=0;i<n;i++){
-            int a = nums[i];
-            int more = target - nums[i];
-            if(mp.find(more) != mp.end()){
-                return {mp[more], i};
-            }
-            mp[nums[i]] = i;
+
+        vector<pair<int, int>> temp(n);
+
+        for(int i = 0; i < n; i++){
+            temp[i] = {nums[i], i};
         }
+
+        sort(temp.begin(), temp.end());
+
+        int i = 0;
+        int j = n-1;
+
+        while(i < j){
+            int sum = temp[i].first + temp[j].first;
+
+            if(sum == target)
+            return {temp[i].second, temp[j].second};
+
+            else if(sum < target){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+
         return {-1, -1};
     }
 };
