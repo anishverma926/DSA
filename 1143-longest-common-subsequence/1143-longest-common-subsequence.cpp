792d766f24793a5b7f0263ffbc395ee7207c6fa1
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-18 09:18:07 +0530

    Time: 68 ms (5.04%), Space: 27.8 MB (6.11%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     // we got the tle because of we did not declare as a reference
     int solve(int i, int j, string& s1, string& s2, vector<vector<int>>& dp){
-        if(i < 0 || j < 0)
+        if(i == 0 || j == 0)
         return 0;
 
         if(dp[i][j] != -1)
@@ -11,7 +11,7 @@ public:
         int not_match = max(solve(i-1, j, s1, s2, dp), solve(i, j-1, s1, s2, dp));
 
         int match = 0;
-        if(s1[i] == s2[j]){
+        if(s1[i - 1] == s2[j - 1]){
             match = 1 + solve(i - 1, j - 1, s1, s2, dp);
         }
 
@@ -22,8 +22,8 @@ public:
         int m = text1.size();
         int n = text2.size();
 
-        vector<vector<int>> dp(m, vector<int> (n, -1));
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
 
-        return solve(m-1, n-1, text1, text2, dp);
+        return solve(m, n, text1, text2, dp);
     }
 };
