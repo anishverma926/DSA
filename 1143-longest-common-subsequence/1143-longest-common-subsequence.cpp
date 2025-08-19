136d7f50c5f8cdbe1b0f380214432230db07f2db
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-19 08:12:44 +0530

    Time: 19 ms (85.91%), Space: 9 MB (96.6%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    // we got the tle because of we did not declare as a reference
-    int solve(int i, int j, string& s1, string& s2, vector<vector<int>>& dp){
-        if(i == 0 || j == 0)
-        return 0;
-
-        if(dp[i][j] != -1)
-        return dp[i][j];
-
-        int not_match = max(solve(i-1, j, s1, s2, dp), solve(i, j-1, s1, s2, dp));
-
-        int match = 0;
-        if(s1[i - 1] == s2[j - 1]){
-            match = 1 + solve(i - 1, j - 1, s1, s2, dp);
-        }
-
-        return dp[i][j] = max(match, not_match);
-    }
-
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size();
         int n = text2.size();
 
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
 
-        return solve(m, n, text1, text2, dp);
+        for(int i = 0; i <= m; i++)
+        curr[0] = 0;
+
+        for(int j = 0; j <= n; j++)
+        prev[j] = 0;
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                int not_match = max(prev[j], curr[j-1]);
+
+                int match = 0;
+                if(text1[i - 1] == text2[j - 1]){
+                    match = 1 + prev[j - 1];
+                }
+
+                curr[j] = max(match, not_match);
+            }
+            prev = curr;
+        }
+
+        return prev[n];
     }
 };
