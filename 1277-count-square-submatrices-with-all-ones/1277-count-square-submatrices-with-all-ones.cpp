9b0c846ccb3d28696f5544c4cddc6363aa0315f0
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-20 10:21:21 +0530

    Time: 3 ms (82.96%), Space: 30.4 MB (48.39%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        int m = grid.size();
-        int n = grid[0].size();
-
-        if(i == m || j == n)
-        return 0;
-
-        if(grid[i][j] == 0)
-        return 0;
-
-        if(dp[i][j] != -1)
-        return dp[i][j];
-
-        int right = solve(i, j + 1, grid, dp);
-        int diagonal = solve(i + 1, j + 1, grid, dp);
-        int left = solve(i + 1, j, grid, dp);
-
-        return dp[i][j] = 1 + min({right, diagonal, left});
-    }
-
     int countSquares(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>>dp(n, vector<int>(m, 0));
+
+        for(int i=0;i<n;i++){
+            dp[i][0] = matrix[i][0];
+        }
+        for(int j=0;j<m;j++){
+            dp[0][j] = matrix[0][j];
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j] == 0){
+                    dp[i][j] = 0;
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1]));
+                }
+            }
+        }
 
         int ans = 0;
-
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(matrix[i][j] == 1)
-                ans += solve(i, j, matrix, dp);
-
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans += dp[i][j];
             }
         }
 
