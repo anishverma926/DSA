a8b33bab4e0fa8310bd114bd7d592f8d9e5d2b1c
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-23 12:23:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        set<vector<int>> set;  // for storing the all answer elements
+
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                unordered_set<int> st;  // for checking the 4th element
+                for(int k = j + 1; k < n; k++){
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k];
+
+                    int fourth = target - sum;
+                    
+                    if(st.count(fourth)){
+                        vector<int>temp = {nums[i], nums[j], nums[k], fourth};
+                        sort(temp.begin(), temp.end());
+                        set.insert(temp);
+                    }
+
+                    st.insert(nums[k]);
+                }
+            }
+        }
+
+        vector<vector<int>> ans(set.begin(), set.end());
+
+        return ans;
+    }
+};
