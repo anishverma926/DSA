4f7bbf55b7d4ca9dd08130c14052adf8a8b85286
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-08-31 15:42:25 +0530

    Time: 0 ms (100%), Space: 77.3 MB (13.03%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        int carry = 0;
+        int sum = 0;
+
+        while(l1 && l2){
+            sum = carry + (l1 -> val + l2 -> val);
+
+            // if sum greater than 10 then add (sum % 10)
+
+            temp -> next = new ListNode(sum % 10); // sum = 12 then 12 % 10 = 2 
+            carry = sum / 10;  // then carry 12 / 10 = 1
+
+            temp = temp -> next;
+            l1 = l1 -> next;
+            l2 = l2 -> next;
+        }
+
+        while(l1){
+            sum = carry + l1 -> val;
+
+            temp -> next = new ListNode(sum % 10);
+            carry = (sum) / 10;
+
+            temp = temp -> next;
+            l1 = l1 -> next;
+        }
+
+        while(l2){
+            sum = carry + l2 -> val;
+
+            temp -> next = new ListNode(sum % 10);
+            carry = (sum) / 10;
+
+            temp = temp -> next;
+            l2 = l2 -> next;
+        }
+
+        if(carry > 0)
+        temp -> next = new ListNode(carry);
+
+        return dummyNode -> next;
+    }
+};
