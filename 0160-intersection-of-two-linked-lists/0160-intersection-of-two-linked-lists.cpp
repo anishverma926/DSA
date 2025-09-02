73730e37df0907dead3d11621828ed9a3b3966fd
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-09-02 10:25:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,41 +8,26 @@
  */
 class Solution {
 public:
-    ListNode* solve(ListNode* l1, ListNode* l2, int diff){
-        for(int i = 0; i < diff; i++){
-            l1 = l1 -> next;
-        }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* l1 = headA;
+        ListNode* l2 = headB;
 
-        while(l1 && l2){
+        // if(!l1 || !l2)
+        // return NULL;
+
+        while(l1 != l2){
+            l1 = l1 -> next;
+            l2 = l2 -> next;
+
             if(l1 == l2)
             return l1;
 
-            l1 = l1 -> next;
-            l2 = l2 -> next;
+            if(!l1) l1 = headB;
+            if(!l2) l2 = headA;
+
+
         }
 
         return NULL;
-    }
-
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int n1 = 0, n2 = 0;
-        ListNode* temp1 = headA;
-        ListNode* temp2 = headB;
-
-        while(temp1){
-            n1++;
-            temp1 = temp1 -> next;
-        }
-
-        while(temp2){
-            n2++;
-            temp2 = temp2 -> next;
-        }
-
-        if(n1 > n2){
-            return solve(headA, headB, n1 - n2);
-        }
-        else
-        return solve(headB, headA, n2 - n1); // pass greater list as l1
     }
 };
