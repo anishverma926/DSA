a1f1be30af2e9d2298019b086f88afe5a581dfaf
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-10-12 22:53:55 +0530

    Time: 0 ms (100%), Space: 14.6 MB (48.23%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,70 +1,75 @@
 class Solution {
 public:
-
-    int row[4] = {-1,1,0,0};
-    int col[4] = {0,0,1,-1};
+    int row[4] = {-1, 0, 0, 1};
+    int col[4] = {0, -1, 1, 0};
     
-    bool valid(int i, int j, int n, int m){
-        return (i>=0 && i<n) && (j>=0 && j<m);
+    bool valid(int i, int j, int m, int n){
+        return (i >= 0 && i < m) && (j >= 0 && j < n);
     }
 
     void solve(vector<vector<char>>& board) {
-        int n = board.size();
-        int m = board[0].size();
+        int m = board.size();
+        int n = board[0].size();
 
-        queue<pair<int, int>>q;
+        queue<pair<int, int>> q;
+
         // 1st row
-        for(int j=0;j<m;j++){
+        for(int j = 0; j < n; j++){
             if(board[0][j] == 'O'){
-               q.push({0,j});
-               board[0][j] = 'T';
-            }
-        }
-        // 1st col
-        for(int i=0;i<n;i++){
-            if(board[i][0] == 'O'){
-               q.push({i,0});
-               board[i][0] = 'T';
-            }
-        }
-        // last row
-        for(int j=1;j<m;j++){
-            if(board[n-1][j] == 'O'){
-               q.push({n-1,j});
-               board[n-1][j] = 'T';
-            }
-        }
-        // last col
-        for(int i=1;i<n-1;i++){
-            if(board[i][m-1] == 'O'){
-               q.push({i,m-1});
-               board[i][m-1] = 'T';
+                q.push({0, j});
+                board[0][j] = 'T';
             }
         }
 
+        // 1st col
+        for(int i = 1; i < m; i++){
+            if(board[i][0] == 'O'){
+                q.push({i, 0});
+                board[i][0] = 'T';
+            }
+        }
+
+        // last row
+        for(int j = 1; j < n; j++){
+            if(board[m-1][j] == 'O'){
+                q.push({m-1, j});
+                board[m-1][j] = 'T';
+            }
+        }
+
+        // last col
+        for(int i = 1; i < m-1; i++){
+            if(board[i][n-1] == 'O'){
+                q.push({i, n-1});
+                board[i][n-1] = 'T';
+            }
+        }
+
+        // check connected 'O' with boundaries 'O'
         while(!q.empty()){
             int i = q.front().first;
             int j = q.front().second;
             q.pop();
             
-            for(int k=0;k<4;k++){
-                int new_i = i+row[k];
-                int new_j = j+col[k];
+            for(int k = 0; k < 4; k++){
+                int ni = i + row[k];
+                int nj = j + col[k];
                 
-                if(valid(new_i, new_j, n,m) && board[new_i][new_j] == 'O'){
-                    board[new_i][new_j] = 'T';
-                    q.push({new_i, new_j});
+                if(valid(ni, nj, m, n) && board[ni][nj] == 'O'){
+                    board[ni][nj] = 'T';
+                    q.push({ni, nj});
                 }
             }
         }
-        
-        for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++){
-            if(board[i][j] == 'O')
-            board[i][j] = 'X';
-            else if(board[i][j] == 'T')
-            board[i][j] = 'O';
-        }
 
+        // convert all remaining 'O' to 'X'
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 'O')
+                board[i][j] = 'X';
+                else if(board[i][j] == 'T')
+                board[i][j] = 'O';
+            }
+        }
     }
 };
